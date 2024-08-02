5aea2a946e317d8bd409f05cc037bbe0848e83ce
Abheelash-Mishra <abheelashmishra@gmail.com>
2024-08-02 18:40:10 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import axios from 'axios';
+import Select from "react-select";
+
+const App = () => {
+	const [input, setInput] = useState('');
+	const [error, setError] = useState('');
+	const [response, setResponse] = useState(null);
+	const [selectedOptions, setSelectedOptions] = useState([]);
+
+	const options = [
+		{ value: 'alphabets', label: 'Alphabets' },
+		{ value: 'numbers', label: 'Numbers' },
+		{ value: 'highest_alphabet', label: 'Highest alphabet' }
+	];
+
+	const handleInputChange = (e) => {
+		setInput(e.target.value);
+		setResponse(null);
+		setSelectedOptions([]);
+		setError('');
+	};
+
+	const handleSubmit = async () => {
+		try {
+			const parsedInput = JSON.parse(input);
+			if (!parsedInput.data || !Array.isArray(parsedInput.data)) {
+				throw new Error('Invalid JSON format');
+			}
+
+			const res = await axios.post('https://bajaj-backend-myao6svy8-abheelash-mishras-projects.vercel.app/bfhl', parsedInput);
+			setResponse(res.data);
+			console.log(res.data);
+		} catch (err) {
+			setError('Invalid JSON format or error in API call');
+		}
+	};
+
+	const handleSelectChange = (selected) => {
+		const values = selected.map(option => option.value);
+		setSelectedOptions(values);
+		console.log(values);
+	};
+
+
+	return (
+		<div className={ "h-[80vh] flex flex-col items-center justify-center" }>
+
+			<div className={ "w-1/2 flex flex-col items-center" }>
+				<label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
+				<textarea
+					id="message"
+					value={input}
+					onChange={handleInputChange}
+					rows="8"
+					className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+					placeholder="Enter the JSON data here..."
+				/>
+
+				<button
+					type="button"
+					className="text-white mt-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+					onClick={handleSubmit}
+				>
+					Submit
+				</button>
+			</div>
+
+			{error && <div className="error">{error}</div>}
+
+			{response && (
+				<>
+					<Select
+						isMulti
+						options={ options }
+						onChange={ handleSelectChange }
+					/>
+
+					<div className="response">
+						{ selectedOptions.includes('alphabets') && (
+							<div>
+								<strong>Alphabets:</strong> { JSON.stringify(response.alphabets) }
+							</div>
+						) }
+						{ selectedOptions.includes('numbers') && (
+							<div>
+								<strong>Numbers:</strong> { JSON.stringify(response.numbers) }
+							</div>
+						) }
+						{ selectedOptions.includes('highest_alphabet') && (
+							<div>
+								<strong>Highest Alphabet:</strong> { JSON.stringify(response.highest_alphabet) }
+							</div>
+						) }
+					</div>
+				</>
+			) }
+		</div>
+
+	);
+};
+
+export default App;
---
+++ src/components/TextField.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TextField = ({input}) => {
+	return (
+		<div>
+
+		</div>
+	);
+};
+
+export default TextField;
